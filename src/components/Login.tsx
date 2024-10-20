9245d6f704c08fdcae5148ebee9326942d345933
Amato Luca <amatoroccoluca@gmail.com>
2024-10-20 15:40:27 +0200

    chore: add state maning for email and password

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,55 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('@@ Submitted');
+    console.log('@@ email:', enteredValues.email);
+    console.log('@@ password:', enteredValues.password);
+  };
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    identifier: string
+  ) => {
+    setEnteredValues((prevEnteredValues) => {
+      return {
+        ...prevEnteredValues,
+        [identifier]: event.target.value,
+      };
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredValues.email}
+            onChange={(event) => handleInputChange(event, 'email')}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredValues.password}
+            onChange={(event) => handleInputChange(event, 'password')}
+          />
         </div>
       </div>
 
